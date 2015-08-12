5716777da0e64332571695421e080e30639536a0
Jesse Maxwell <jesse@swiftotter.com>
2015-08-12 12:59:32 -0500

    Initial startup js file created with requirejs

---
+++ js/run.js
@@ -0,0 +1,31 @@
+/**
+ * SwiftOtter_Base is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SwiftOtter_Base is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with SwiftOtter_Base. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright: 2015 (c) SwiftOtter Studios
+ *
+ * @author Jesse Maxwell
+ * @copyright Swift Otter Studios, 8/12/15
+ */
+
+requirejs.config({
+    paths: {
+        "jQuery": "https://code.jquery.com/jquery-2.1.4.min"
+    }
+});
+
+requirejs(["jQuery"],
+    function($) {
+    "use strict";
+
+    console.log("App is running");
+});
